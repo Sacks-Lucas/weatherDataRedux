daf69ad339b33a98cda3a44e556635545c04733a
Sacks-Lucas <sackslm0@gmail.com>
2021-05-26 15:10:16 -0300

    Primer Commit. Codigo Base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+/* eslint-disable no-console */
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import LocationList from './components/LocationList';
+import ForecastExtended from './components/ForecastExtended';
+// import {createStore} from 'redux'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const cities = [
+  'Buenos Aires,ar',
+  'Washington,us',
+  'Bogota,co',
+  'Mexico,mx',
+  'Madrid,es'
+];
+
+// const store= createStore(()=>{})
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = { city: null };
+  }
+
+  handleSelectedLocation = city => {
+    this.setState({ city });
+    console.log(`handleSelectedLocation ${city}`);
+    // const action={type: 'setCity',value:city}
+    // store.dispatch(action)
+  }
+  
+  render() {
+    const { city } = this.state;
+    return (
+
+      <MuiThemeProvider>
+        <Grid>
+          <Row>
+            <Col xs={12}>
+              <AppBar title="Weather App" />
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} md={6}>
+              <LocationList cities={cities} 
+              onSelectedLocation={this.handleSelectedLocation} ></LocationList>
+            </Col>
+            <Col xs={12} md={6}>
+              <Paper zDepth={4}>
+                <div className='detail'>
+                  {
+                    city && 
+                    <ForecastExtended city={city}></ForecastExtended>
+                  }
+
+                </div>
+              </Paper>
+            </Col>
+          </Row>
+        </Grid>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
