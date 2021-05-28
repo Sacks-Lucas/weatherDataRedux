db87c5b4da46e93d6f5fccf3e98a1a31880d8626
Sacks-Lucas <sackslm0@gmail.com>
2021-05-28 15:49:35 -0300

    Commit seccion 36

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -4,20 +4,6 @@ import CircularProgress from 'material-ui/CircularProgress';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css';
-/*
-const api_key = "f99bbd9e4959b513e9bd0d7f7356b38d";
-const url = "http://api.openweathermap.org/data/2.5/weather";
-componentWillMount() {
-    const { city } = this.state;
-    const api_weather = `${url}?q=${city}&appid=${api_key}`;
-    fetch(api_weather).then( data => {
-        return data.json();
-    }).then( weather_data => {
-        const data = transformWeather(weather_data);
-        this.setState({ data });
-    });
-}
-*/
 
 const WeatherLocation = ({onWeatherLocationClick, city, data } )=> {
         return (
@@ -27,8 +13,6 @@ const WeatherLocation = ({onWeatherLocationClick, city, data } )=> {
             <CircularProgress size={60} thickness={7} />}
         </div>);
 };
-
-
 
 WeatherLocation.propTypes = {
     city: PropTypes.string,
--- src/containers/LocationListContainer.js
+++ src/containers/LocationListContainer.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import LocationList from './../components/LocationList';
+import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
-import { setSelectedCity,setWeather } from './../actions';
+// import { setSelectedCity,setWeather } from './../actions';
+import * as actions from './../actions';
 import { Component } from 'react';
-import {getWeatherCities} from '../reducers'
+import {getCity, getWeatherCities} from '../reducers'
 
 class LocationListContainer extends Component {
     
     componentDidMount(){
-        this.props.setWeather(this.props.cities)
+        const {setWeather,setSelectedCity,cities,city} = this.props
+        setWeather(cities)
+        setSelectedCity(city)
     }
 
     handleSelectedLocation = city => {
-        this.props.setCity(city)
+        this.props.setSelectedCity(city)
     }
     render(){
         return (
@@ -24,18 +28,24 @@ class LocationListContainer extends Component {
 }
 
 LocationListContainer.propTypes = {
-    setCity:PropTypes.func.isRequired,
+    // setCity:PropTypes.func.isRequired,
+    setSelectedCity:PropTypes.func.isRequired,
+    setWeather:PropTypes.func.isRequired,
     cities:PropTypes.array.isRequired,
     citiesWeather:PropTypes.array,
+    city:PropTypes.string.isRequired,
 }
 
 const mapStateToProps = state =>({
-    citiesWeather: getWeatherCities(state)
+    citiesWeather: getWeatherCities(state),
+    city: getCity(state)
 })
 
+const mapDispatchToProps= dispatch => bindActionCreators(actions, dispatch)
+/*
 const mapDispatchToProps= dispatch =>({
     setCity: value => dispatch(setSelectedCity(value)),
     setWeather: cities => dispatch(setWeather(cities))
 })
-
+*/
 export default connect(mapStateToProps, mapDispatchToProps)(LocationListContainer)
